f85de364be5b851ef1be731c7564c57a1cebbb06
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-17 19:06:51 +0000

    logging

--- train_speech_embedder.py
+++ train_speech_embedder.py
@@ -42,7 +42,7 @@ def train(model_path):
     iteration = 0
     for e in range(hp.train.epochs):
         total_loss = 0
-        for batch_id, mel_db_batch in tqdm(enumerate(train_loader)): 
+        for batch_id, mel_db_batch in tqdm(enumerate(train_loader), total=len(train_loader)): 
             mel_db_batch = mel_db_batch.to(device)
             
             mel_db_batch = torch.reshape(mel_db_batch, (hp.train.N*hp.train.M, mel_db_batch.size(2), mel_db_batch.size(3)))
